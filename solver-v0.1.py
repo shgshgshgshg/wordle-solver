12d6cc73529ae0b7f0774d9a9801985e2578842b
shgshgshgshg <simonhugogil@gmail.com>
2022-03-03 20:57:31 -0500

    OS detector

--- solver-v0.1.py
+++ solver-v0.1.py
@@ -1,4 +1,11 @@
-file = open("/usr/share/dict/words", "r")
-words = file.readlines()
-file.close()
+import os
+import platform
 
+op_system = platform.system()
+file = None
+words = None
+
+elif:
+    file = open("/usr/share/dict/words", "r")
+    words = file.readlines()
+    file.close()
