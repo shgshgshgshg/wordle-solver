1c4681c7959495165c27ce4fa99b44fe0a8f9551
shgshgshgshg <simonhugogil@gmail.com>
2022-03-07 20:46:15 -0500

    Added letter detection test

--- solver-v0.1.py
+++ solver-v0.1.py
@@ -6,7 +6,7 @@ file.close()
 current = 0
 possible = []
 
-while len(possible) != 10:
+while len(possible) != 158390:
     word = words[current].rstrip()
     if len(word) == 5:
         possible.append(word)
@@ -14,3 +14,15 @@ while len(possible) != 10:
     else:
         current = current + 1
         continue
+
+letter = input('Type a letter in word, regardless of position or capitalization: ')
+
+num_checking = 0
+word_checking = possible[num_checking]
+still_possible = len(possible)
+
+while num_checking != len(possible):
+    if letter in word_checking:
+        continue
+    else:
+        possible.pop(num_checking)
